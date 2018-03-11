3b6a7cae217ab9f4eda13e71c21381e6057f0e77
Brian Nguyen <37276397+brianmtnguyen@users.noreply.github.com>
2018-03-11 16:46:19 -0700

    Update Magpie2.java

--- MagpieActivityStarterCode/activity2/Magpie2.java
+++ MagpieActivityStarterCode/activity2/Magpie2.java
@@ -42,6 +42,23 @@ public class Magpie2
 		{
 			response = "Tell me more about your family.";
 		}
+		else if (statement.indexOf("dog") >= 0
+				|| statement.indexOf("cat") >= 0)
+		{
+			response = "Tell me more about your pets.";
+		}	
+		else if (statement.indexOf("Mr. Reichow") >= 0)
+		{
+			response = "He sounds like a good teacher.";
+		}	
+		else if (statement.trim().length() ==0)
+		{
+			response = "Say something, please.";
+		}
+		else if (statement.indexOf("no") >= 0)
+		{
+			response = "Unfortunate.";
+		}	
 		else
 		{
 			response = getRandomResponse();
@@ -55,7 +72,7 @@ public class Magpie2
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -75,6 +92,14 @@ public class Magpie2
 		else if (whichResponse == 3)
 		{
 			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "Alright.";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "Okay.";
 		}
 
 		return response;
